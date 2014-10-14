ec5cde413e9fd8f31529f48fadc77f21b559df21
saiYu <yujinqiuustb@gmail.com>
2014-10-14 10:41:47 +0800

    import to tagkey

---
+++ controllers/tagkey.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type TagKeyController struct {
+	beego.Controller
+}
+
+func (this *TagKeyController) Get() {
+	this.TplNames = "tree-tag-manager.html"
+}
